a3386440a54ba0890003c6974433b06856a972bc
MartinDP333 <Martindauer448@gmail.com>
2025-05-12 10:49:46 +0200

    Lavede selve layoutet til login siden Hermed er der tilføjet tomme basale kodestykker ind
    (Der skal lige huskes at der måske senere skal tilføjes en Registrer knap, men det er først når vi har fået svar om det er åbent eller lukket system)

---
+++ Semester Projekt 1/Login.cs
@@ -0,0 +1,40 @@
+namespace Semester_Projekt_1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e) // dette er selve formen
+        {
+
+        }
+
+        private void UsernameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
