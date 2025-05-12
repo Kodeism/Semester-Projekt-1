5caa9abd788b9689d7f4c7c746045a294bff4cb5
MartinDP333 <Martindauer448@gmail.com>
2025-05-12 13:26:08 +0200

    Fjernede noget som jeg ikke skulle have med aligevel :p

--- Semester Projekt 1/Login.cs
+++ Semester Projekt 1/Login.cs
@@ -5,8 +5,6 @@ namespace Semester_Projekt_1
         public Login()
         {
             InitializeComponent();
-            var saleRegistration = new SaleRegistration(); // laves til test af sale registration
-            saleRegistration.Show();
         }
 
         private void Login_Load(object sender, EventArgs e) // dette er selve formen
