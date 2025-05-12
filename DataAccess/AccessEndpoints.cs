f6a74113604dd34c317bf703e03fc9313c196473
RubenBaader <rubenbaader@gmail.com>
2025-05-12 10:24:32 +0200

    Created basic architecture

---
+++ DataAccess/AccessEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Repositories;
+
+namespace DataAccess
+{
+    public class AccessEndpoints
+    {
+        private readonly DataRepository repository;
+        string connectionString = "Server = localhost; Database = Semester projekt gruppe 1; User ID = em; Password = 1234; Trusted_Connection = True;";
+
+        public AccessEndpoints()
+        {
+            repository = new DataRepository(connectionString);
+        }
+    }
+}
---
+++ DataAccess/Repositories/DataRepository.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Repositories.Contracts;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess.Repositories
+{
+    internal class DataRepository : IDataRepository
+    {
+        private readonly string connectionString;
+        private readonly SqlConnection connection;
+        public DataRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+            connection = new SqlConnection(connectionString);
+        }
+        public Bolig CreateBolig(Bolig bolig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ejendomsmægler CreateEjendomsmægler(Ejendomsmægler ejendomsmægler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
