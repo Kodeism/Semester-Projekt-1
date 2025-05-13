0562d2952a44b5590e722fb974811a96b5c99795
MartinDP333 <Martindauer448@gmail.com>
2025-05-13 10:18:07 +0200

    Har tllføjet noget meget basal validering af input dette er om input er tomt eller indeholder mellemrum Dette er bare for at gøre så mængden af uproduktive input er reduceret.
    Der er også blevet lagt op til at der kan skrives noget kode omgående query til databasen, men det fatter jeg ikke lige nu så :)

--- BusineesLogic/Class1.cs
+++ BusineesLogic/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace BusineesLogic
+﻿using System.Drawing;
+
+namespace BusineesLogic
 {
     public class Class1
     {
 
+    }
+    public class TryLogin
+    {
+        public bool login(string username, string password)
+        {
+            // kode som laver et login query her
+            // returner enten true eller false ved loginforsøg
+            return true; // for nu er det altid true, men der skal tilføjes en aktuel query her
+        }
+        
     }
 }
